262973de7a9320451f1743ab9c4b4460079d1cfb
Maria Kasińska <mariakasinska17@gmail.com>
2019-12-02 14:33:26 +0100

    add main nav

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import styles from './Header.scss';
+import Container from '../Container/Container';
+import Icon from '../Icon/Icon';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className={styles.component}>
+        <Container>
+          <div className={styles.wrapper}>
+            <Link className={styles.logo} to={'/'}>
+              <Icon name={'clipboard'}></Icon>
+            </Link>
+            <NavLink exact to='/' activeClassName='active'>Home</NavLink>
+            <NavLink exact to='/info' activeClassName='active'>Info</NavLink>
+          </div>
+        </Container>
+      </header>
+    );
+  }
+}
+
+export default Header;
